6e20afd0df4422a91115170e0bba9922621b92d0
zotin <zotinalexander@gmail.com>
2023-02-19 16:38:20 +0100

    commit

---
+++ src/main/java/com/example/kursovaya3kurs/Kursovaya3KursApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursovaya3kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaya3KursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursovaya3KursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursovaya3kurs/Kursovaya3KursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursovaya3kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovaya3KursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
